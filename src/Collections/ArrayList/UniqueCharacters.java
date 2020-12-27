f4daf5f4b82ed8f6c2ead7203c82e9426d00c475
gayatri-gb <gayatribhatnagar8@gmail.com>
2020-12-27 00:58:57 -0600

    Java Collections LeetCode

---
+++ src/Collections/ArrayList/UniqueCharacters.java
@@ -0,0 +1,20 @@
+package Collections.ArrayList;
+
+// Implement an algorithm to determine if a string has all unique characters. What
+//if you cannot use additional data structures?
+public class UniqueCharacters {
+
+    static  public boolean isUnique(String str){
+        //ASCII length=128
+        if(str.length()>128){
+            return false;
+        }
+
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
